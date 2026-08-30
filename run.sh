4dfc25b42e7ce77aa2b113af5adfdbf182f33456
Solomon Jacobs <solomonjacobs@protonmail.com>
2026-08-30 10:28:51 +0200

    output runtimes

--- run.sh
+++ run.sh
@@ -7,16 +7,16 @@ FLUSHES=$PWD/alertgen-flushes.jsonl.gz
 
 go test -v -count=1 ./inhibit -run TestReplay -config.file=$CONFIG -recording.file=$RECORDING -flush.file=$FLUSHES > /tmp/inhibit.log 2>&1
 echo "=== inhibit"
-grep -E 'flushes:|Mutes calls|alerts muted|fully muted|FAIL' /tmp/inhibit.log | sed 's/.*replay_test.go:[0-9]*: //'
+grep -E 'flushes:|Mutes calls|alerts muted|fully muted|FAIL|^--- (PASS|FAIL)|^ok ' /tmp/inhibit.log | sed 's/.*replay_test.go:[0-9]*: //'
 
 go test -v -count=1 ./patchedinhibit -run TestReplay -config.file=$CONFIG -recording.file=$RECORDING -flush.file=$FLUSHES > /tmp/patchedinhibit.log 2>&1
 echo "=== patchedinhibit"
-grep -E 'flushes:|Mutes calls|alerts muted|fully muted|FAIL' /tmp/patchedinhibit.log | sed 's/.*replay_test.go:[0-9]*: //'
+grep -E 'flushes:|Mutes calls|alerts muted|fully muted|FAIL|^--- (PASS|FAIL)|^ok ' /tmp/patchedinhibit.log | sed 's/.*replay_test.go:[0-9]*: //'
 
 go test -v -count=1 ./v29inhibit -run TestReplay -config.file=$CONFIG -recording.file=$RECORDING -flush.file=$FLUSHES > /tmp/v29inhibit.log 2>&1
 echo "=== v29inhibit"
-grep -E 'flushes:|Mutes calls|alerts muted|fully muted|FAIL' /tmp/v29inhibit.log | sed 's/.*replay_test.go:[0-9]*: //'
+grep -E 'flushes:|Mutes calls|alerts muted|fully muted|FAIL|^--- (PASS|FAIL)|^ok ' /tmp/v29inhibit.log | sed 's/.*replay_test.go:[0-9]*: //'
 
 go test -v -count=1 ./5449updatedinhibit -run TestReplay -config.file=$CONFIG -recording.file=$RECORDING -flush.file=$FLUSHES > /tmp/5449updatedinhibit.log 2>&1
 echo "=== 5449updatedinhibit"
-grep -E 'flushes:|Mutes calls|alerts muted|fully muted|FAIL' /tmp/5449updatedinhibit.log | sed 's/.*replay_test.go:[0-9]*: //'
+grep -E 'flushes:|Mutes calls|alerts muted|fully muted|FAIL|^--- (PASS|FAIL)|^ok ' /tmp/5449updatedinhibit.log | sed 's/.*replay_test.go:[0-9]*: //'
