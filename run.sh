32d706dcbb56e952d36628141c079f91d2f9b592
Solomon Jacobs <solomonjacobs@protonmail.com>
2026-08-30 10:25:25 +0200

    check 5449 only

--- run.sh
+++ run.sh
@@ -16,3 +16,7 @@ grep -E 'flushes:|Mutes calls|alerts muted|fully muted|FAIL' /tmp/patchedinhibit
 go test -v -count=1 ./v29inhibit -run TestReplay -config.file=$CONFIG -recording.file=$RECORDING -flush.file=$FLUSHES > /tmp/v29inhibit.log 2>&1
 echo "=== v29inhibit"
 grep -E 'flushes:|Mutes calls|alerts muted|fully muted|FAIL' /tmp/v29inhibit.log | sed 's/.*replay_test.go:[0-9]*: //'
+
+go test -v -count=1 ./5449updatedinhibit -run TestReplay -config.file=$CONFIG -recording.file=$RECORDING -flush.file=$FLUSHES > /tmp/5449updatedinhibit.log 2>&1
+echo "=== 5449updatedinhibit"
+grep -E 'flushes:|Mutes calls|alerts muted|fully muted|FAIL' /tmp/5449updatedinhibit.log | sed 's/.*replay_test.go:[0-9]*: //'
